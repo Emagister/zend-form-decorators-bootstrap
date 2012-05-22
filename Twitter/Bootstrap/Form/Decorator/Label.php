0dc222a353fe8aaed8e18171868adcdb9bc393ad
David Weinraub <david@papayasoft.com>
2012-05-22 18:23:03 +0700

    Clean docblocks

--- Twitter/Bootstrap/Form/Decorator/Label.php
+++ Twitter/Bootstrap/Form/Decorator/Label.php
@@ -1,16 +1,34 @@
 <?php
 
 /**
- * Description of Label
+ * Label decorator definition
  *
- * @author David Weinraub <david@papayasoft.com>
+ * @category Forms
+ * @package Twitter_Bootstrap_Form_Decorator
+ * @subpackage Label
+ * @author David Weinraub <david@papyasoft.com>
+ */
+
+/**
+ * Renders the label for an element
+ *
+ * @category Forms
+ * @package Twitter_Bootstrap_Form_Decorator
+ * @subpackage Label
+ * @author David Weinraub <david@papyasoft.com>
  */
 class Twitter_Bootstrap_Form_Decorator_Label extends Zend_Form_Decorator_Label
 {
+    /**
+     * Render the label. Suppress label rendering for submit buttons.
+     * 
+     * @param string $content 
+     * @return string
+     */
     public function render($content)
     {
         $element = $this->getElement();
-        if ($element instanceof Zend_Form_Element_Submit){
+        if ($element instanceof Zend_Form_Element_Submit) {
             return $content;
         } else {
             return parent::render($content);
