8d5b4ace6c00d8f59985ca41cdf71e978a503941
Lee Robert <lrobert@tangentmtw.com>
2012-08-22 13:55:07 -0400

    Added an element for uneditable text

---
+++ Twitter/Bootstrap/Form/Element/UneditableTextfield.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * A form button submit definition
+ *
+ * @category Forms
+ * @package Twitter_Bootstrap_Form
+ * @subpackage Element
+ * @author Christian Soronellas <csoronellas@emagister.com>
+ */
+
+/**
+ * A form submit button
+ *
+ * @category Forms
+ * @package Twitter_Bootstrap_Form
+ * @subpackage Element
+ * @author Christian Soronellas <csoronellas@emagister.com>
+ */
+class Twitter_Bootstrap_Form_Element_UneditableTextfield extends Zend_Form_Element_Text
+{
+    /**
+     * Use formButton view helper by default
+     * 
+     * @var string
+     */
+    public $helper = 'formUneditableTextfield';
+}
